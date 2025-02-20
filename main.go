b39e0b2b29ee5862d3675f21174bc283b20b78ff
Nicholas <nicholaszingleman@gmail.com>
2025-02-20 12:49:28 -0500

    using /app/assets/ for file server

--- main.go
+++ main.go
@@ -6,20 +6,39 @@ import (
 )
 
 const (
-	port = ":8080"
-	root = "/"
+	port = "8080"
+
+	rootPath  = "/"
+	appPath   = "/app/"
+	readyPath = "/healthz"
+
+	// HTTP Status Codes
+	OK = 200
+
+	ServiceUnavailable = 503
 )
+
+func handlerReady(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(OK)
+	writer.Write([]byte("OK"))
+}
 
 func main() {
 
-	serveMux := http.NewServeMux()
-	serveMux.Handle(root, http.FileServer(http.Dir(".")))
+	mux := http.NewServeMux()
+
+	// file handler
+	mux.Handle(appPath, http.StripPrefix(appPath, http.FileServer(http.Dir("."))))
+
+	// health/ready handler
+	mux.HandleFunc(readyPath, handlerReady)
 
 	server := http.Server{
-		Addr:    port,
-		Handler: serveMux,
+		Addr:    ":" + port,
+		Handler: mux,
 	}
 
 	log.Printf("Listening and Serving on port: '%s'\n", port)
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe()) // server can return error
 }
