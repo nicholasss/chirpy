4e1231b86626c680b5e787b1961bad60d0e5b487
Nicholas <nicholaszingleman@gmail.com>
2025-02-22 14:29:42 -0500

    renamed writer, req, to w, r for consistency, and added comment for handlerValidate

--- main.go
+++ main.go
@@ -28,41 +28,41 @@ type apiConfig struct {
 }
 
 func (cfg *apiConfig) mwLog(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
-		log.Printf("%s %s", req.Method, req.URL.Path)
-		next.ServeHTTP(writer, req)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
 	})
 }
 
 func (cfg *apiConfig) mwMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// this closure will be called when a request is processed
 		cfg.fileserverHits.Add(1)
 		log.Printf("Incremented the fileserver hit counter by 1.")
 
 		// the request is then passed to the next handler in the chain
-		next.ServeHTTP(writer, req)
+		next.ServeHTTP(w, r)
 	})
 }
 
-func (cfg *apiConfig) handlerReset(writer http.ResponseWriter, req *http.Request) {
-	writer.WriteHeader(OK)
-	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(OK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
 	cfg.fileserverHits.Store(0)
 	str := "Reset the fileserver hit counter."
-	writer.Write([]byte(str))
+	w.Write([]byte(str))
 
 	log.Println(str)
 }
 
-func (cfg *apiConfig) handlerMetrics(writer http.ResponseWriter, req *http.Request) {
-	writer.WriteHeader(OK)
-	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(OK)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	hits := cfg.fileserverHits.Load()
 	str := fmt.Sprintf(adminMetricsPage, hits)
-	writer.Write([]byte(str))
+	w.Write([]byte(str))
 
 	log.Printf("Served metrics page with %d hits.", hits)
 }
@@ -75,14 +75,15 @@ func handlerFS(path string) http.Handler {
 	return handler
 }
 
-func handlerReady(writer http.ResponseWriter, req *http.Request) {
-	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	writer.WriteHeader(OK)
-	writer.Write([]byte("OK"))
+func handlerReady(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(OK)
+	w.Write([]byte("OK"))
 
 	log.Printf("Served health page.")
 }
 
+// Accepts POST and expects a json object of a particulate shape
 func handlerValidate(w http.ResponseWriter, req *http.Request) {
 
 }
