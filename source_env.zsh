388739d6b0435508102b8a301b84b5209e4171ff
Nicholas <nicholaszingleman@gmail.com>
2025-02-23 14:57:53 -0500

    script for activating the venv

--- source_env.zsh
+++ source_env.zsh
@@ -1,11 +1,19 @@
 #!/usr/bin/zsh
 
 # sources the local .env file
-
 if [ -e "./.env" ]; then
 	print "Found local '.env' file. Sourcing..."
 	source ./.env
 else
 	print "No local '.env' file found."
+	print "Are you in the right directory, or is it missing?"
+fi
+
+# activates the venv environment
+if [ -d "./venv" && -e "./venv/bin/activate" ]; then
+	print "Found local './venv/' directory. Activating..."
+	/usr/bin/bash ./venv/bin/activate
+else
+	print "No local './venv/' directory found."
 	print "Are you in the right directory, or is it missing?"
 fi
