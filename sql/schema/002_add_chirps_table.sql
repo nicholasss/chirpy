352ba2659b8235d4262d5fe42098442d1c357005
Nicholas <nicholaszingleman@gmail.com>
2025-03-10 17:14:40 -0400

    added chirps table and on delete cascade for the users table, if the user is removed

---
+++ sql/schema/002_add_chirps_table.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+create table chirps (
+	id uuid primary key,
+	created_at timestamp not null,
+	updated_at timestamp not null,
+	body text not null,
+	user_id uuid not null,
+	
+	constraint fk_user
+	foreign key(user_id)
+	references users(id)
+	on delete cascade
+);
+
+-- -goose Down
+drop table chirps;
