ab9ac5468d698fbcceeae68e446d1a1468dbdd25
Nicholas <nicholaszingleman@gmail.com>
2025-03-17 17:26:03 -0400

    added jwt methods and tests, seems to be working

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,8 +1,12 @@
 package auth
 
 import (
+	"fmt"
 	"log"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -25,4 +29,60 @@ func CheckPasswordHash(password, hash string) error {
 	}
 
 	return nil
+}
+
+// creates and returns a JWT
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	currentTime := time.Now().UTC()
+
+	signingMethod := jwt.SigningMethodHS256
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  &jwt.NumericDate{Time: currentTime},
+		ExpiresAt: &jwt.NumericDate{Time: currentTime.Add(expiresIn)},
+		Subject:   userID.String(),
+	}
+	token := jwt.NewWithClaims(signingMethod, claims)
+
+	// HMAC signing method requires the type []byte
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		log.Printf("Error signing JWT: %s", err)
+		return "", err
+	}
+
+	// printing signed token
+	log.Printf("Generated new token: %s", signedToken)
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, &claims,
+		func(token *jwt.Token) (any, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return uuid.Nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(tokenSecret), nil
+		},
+	)
+	if err != nil {
+		log.Printf("Error validating JWT: %v", err)
+		return uuid.Nil, err
+	}
+
+	userID, err := token.Claims.GetSubject()
+	if err != nil {
+		log.Printf("Error accessing token claims: %v", err)
+		return uuid.Nil, err
+	}
+
+	userUUID, err := uuid.Parse(userID)
+	if err != nil {
+		log.Printf("Error parsing userID from token claim: %v", err)
+		return uuid.Nil, err
+	}
+
+	return userUUID, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,7 +2,9 @@ package auth_test
 
 import (
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/nicholasss/chirpy/internal/auth"
 )
 
@@ -23,6 +25,42 @@ func TestHashPassword(t *testing.T) {
 
 		if auth.CheckPasswordHash(test.input, actual) != nil {
 			t.Error("Hash provided does not match.")
+		}
+	}
+}
+
+func TestJWT(t *testing.T) {
+	tests := []struct {
+		inputUUID   uuid.UUID
+		inputSecret string
+	}{
+		{
+			inputUUID:   uuid.New(),
+			inputSecret: "new secret",
+		},
+		{
+			inputUUID:   uuid.New(),
+			inputSecret: "20f181b52ec1eeb31b2ad0",
+		},
+	}
+
+	for _, test := range tests {
+		duration := 1 * time.Minute
+
+		// create token
+		actualToken, err := auth.MakeJWT(test.inputUUID, test.inputSecret, duration)
+		if err != nil {
+			t.Error("unable to create JWT")
+		}
+
+		// compare token
+		actualUUID, err := auth.ValidateJWT(actualToken, test.inputSecret)
+		if err != nil {
+			t.Errorf("unable to validate jwt: %s", err)
+		}
+
+		if actualUUID != test.inputUUID {
+			t.Errorf("Expected: '%s', Got: '%s'", actualUUID.String(), test.inputUUID.String())
 		}
 	}
 }
