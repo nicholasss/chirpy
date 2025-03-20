ca94495f108905c00e22d07ef4baa82833405b71
Nicholas <nicholaszingleman@gmail.com>
2025-03-20 12:46:17 -0400

    create queries for refresh tokens table

---
+++ sql/queries/refresh_tokens.sql
@@ -0,0 +1,18 @@
+-- name: CreateRefreshToken :one
+insert into refresh_tokens (
+  id, created_at, updated_at, user_id, expires_at
+) values (
+  gen_random_uuid(), now(), now(), $1, $2
+)
+returning *;
+
+-- name: GetUserFromRefreshToken :one
+select * from refresh_tokens
+where id = $1;
+
+-- name: RevokeRefreshTokenForUser :exec
+update refresh_tokens
+set
+  updated_at = now(),
+  expires_at = now()
+where id = $1;
